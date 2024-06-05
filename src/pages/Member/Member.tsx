a4c9de2265ae0000de45a8e1566d5e0f80241ac9
24180 <2418013844@qq.com>
2024-06-05 17:58:21 +0800

    update

--- src/pages/Member/Member.tsx
+++ src/pages/Member/Member.tsx
@@ -7,14 +7,17 @@ import imageJiaBoFang from "../../assets/profile/Image_JiaBoFang.png"
 import imageLiZiWen from "../../assets/profile/Image_LiZiWen.png"
 import imageSunYiXuan from "../../assets/profile/Image_SunYiXuan.png"
 import imageZhangHaoYang from "../../assets/profile/Image_ZhangHaoYang.png"
+import imageYangHongYi from "../../assets/profile/Image_YangHongYi.png"
+import imageWangYiZhen from "../../assets/profile/Image_WangYiZhen.png"
 import iconGithub from "../../assets/icon/MdiGithub.svg"
 import iconEmail from "../../assets/icon/MdiEmail.svg"
+
 export function Member() {
     return (<div className={styles.center}>
         <h1 className={styles.main}>团队介绍</h1>
         <h2 className={styles.part}>教师</h2>
         <div className={styles.row}>
-            <MemberDisplay imageUrl={""} name={"教师1"} description={"这是介绍"}
+            <MemberDisplay imageUrl={""} name={"教师"} description={""}
                            link={""}/>
         </div>
         <h2 className={styles.part}>学生</h2>
@@ -22,40 +25,39 @@ export function Member() {
         <div className={styles.row}>
             <MemberDisplay imageUrl={imageWeiJingZhen}
                            name={"魏敬臻"}
-                           description={[]}
+                           description={["感兴趣研究内容:数据分析、强化学习", "负责内容:无人机 多智能体协同"]}
                            link={"https://github.com/Jaychenwei"}
                            icons={[iconEmail,iconGithub,]} iconLinks={["mailto:w21411711151118@outlook.com","https://github.com/Jaychenwei"]}/>
             <MemberDisplay imageUrl={imageJiaBoFang}
                            name={"贾博方"}
-                           description={[]}
+                           description={["感兴趣研究内容:机器人学习", "负责内容:视觉导航 强化学习"]}
                            link={"https://github.com/BofangJia1227"}
                            icons={[iconEmail,iconGithub,]} iconLinks={["mailto:jiabofang@email.swu.edu.cn","https://github.com/BofangJia1227"]}/>
         </div>
 
-        { //暂无2022级的成员 所以使用false控制此板块不显示
-            false&&<>
-            <h3 className={styles.grade}>2022级</h3>
-            <div className={styles.row}>
-                <MemberDisplay imageUrl={""} name={"学生1"} description={"这是介绍"}
-                               link={"点击名字跳转的链接"}/>
-                <MemberDisplay imageUrl={""} name={"学生2"} description={"这是介绍"}
-                               link={"点击名字跳转的链接"}/>
-            </div>
-            </>
-        }
 
+        <h3 className={styles.grade}>2022级</h3>
+            <div className={styles.row}>
+                <MemberDisplay imageUrl={imageWangYiZhen} name={"王怡臻"} description={[]}
+                               link={""}
+                               icons={[iconEmail,iconGithub]}
+                               iconLinks={["mailto:3131890024@qq.com","https://github.com/dashboard"]}
+                />
+            </div>
         <h3 className={styles.grade}>2023级</h3>
         <div className={styles.row}>
             <MemberDisplay imageUrl={imageLiZiWen}
                            name={"李梓闻"}
-                           description={[]}
+                           description={["感兴趣方向:强化学习","负责内容:智能车开发、仿真环境搭建"]}
                            link={""}
                            icons={[iconEmail,]}
                            iconLinks={["mailto:19823000405@163.com"]}/>
-            <MemberDisplay imageUrl={""}
+            <MemberDisplay imageUrl={imageYangHongYi}
                            name={"杨弘毅"}
                            description={""}
-                           link={""}/>
+                           link={""}
+
+            />
         </div>
         <h2 className={styles.part}>合作者</h2>
         <div className={styles.row}>
@@ -67,13 +69,13 @@ export function Member() {
                            iconLinks={["mailto:2418013844@qq.com","https://github.com/aquamarine-z"]}/>
             <MemberDisplay imageUrl={imageCuiMengKe}
                            name={"崔梦柯"}
-                           description={[]}
+                           description={["感兴趣研究内容:图像处理 图神经网络GNN"]}
                            link={"https://github.com/cuimengke/"}
                            icons={[iconEmail,iconGithub,]}
                            iconLinks={["mailto:1844755515@qq.com","https://github.com/cuimengke/"]}/>
             <MemberDisplay imageUrl={imageHuZiAng}
                            name={"胡子昂"}
-                           description={[]}
+                           description={["感兴趣开发内容:工程开发、硬件、人工智能"]}
                            link={""}
                            icons={[iconEmail,]}
                            iconLinks={["mailto:hza2002@foxmail.com"]}/>
